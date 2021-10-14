bafaa74cce95e2e31745f18b15ec0bb87e80446d
anthony-freitas <78743473+anthony-freitas@users.noreply.github.com>
2021-10-14 08:56:50 -0300

    again fixinf

--- setup.py
+++ setup.py
@@ -9,5 +9,10 @@ setup(
     description='Miscellaneous Library to Simplify Python Code',
     author='anthony',
     url='https://github.com/anthony-freitas/py-misc',
-    packages=['py_misc','py_misc/_call','py_misc/_threading','py_misc/_time']
+    packages=[
+        'py_misc',
+        'py_misc/_call',
+        'py_misc/_threading',
+        'py_misc/_time'
+    ]
 )
