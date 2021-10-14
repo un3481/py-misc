002dffe3c466dd675379d6faeb0b618f1ae09663
anthony-freitas <78743473+anthony-freitas@users.noreply.github.com>
2021-10-14 10:28:02 -0300

    fix __main__ self refrences

--- py_misc/__init__.py
+++ py_misc/__init__.py
@@ -88,21 +88,21 @@ from ._misc import Misc
 ##########################################################################################################################
 
 # Get Location of Main Script
-def __schema__(self):
-    path = self.sys.modules['__main__'].__file__
-    path = self.os.path.abspath(path)
+def __schema__():
+    path = sys.modules['__main__'].__file__
+    path = os.path.abspath(path)
     return path
 
 # Get Reference Object
-def proxy_reference(self, obj):
+def proxy_reference(obj):
     reference = [obj]
     while hasattr(reference[-1], '__proxy__'):
         reference.append(reference[-1].__proxy__)
     return reference[-1]
 
 # Get Location of Object in Script
-def locale(self, obj):
-    reference = self.proxy_reference(obj)
+def locale(obj):
+    reference = proxy_reference(obj)
     module = reference.__module__ if hasattr(reference, '__module__') else '__main__'
     module = module if module.startswith('__main__') else '::'.join(('__main__', module))
     qual = reference.__qualname__ if hasattr(reference, '__qualname__') else '__?__'
@@ -111,7 +111,7 @@ def locale(self, obj):
     return locale
 
 # Keep Alive
-def keepalive(self):
+def keepalive():
     try: # Keep Main Thread Alive
         while True: pass
     except KeyboardInterrupt: pass
