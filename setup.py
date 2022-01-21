2980e0880865fc58d6af568f480be046bec66ab5
anthony <78743473+melon-yellow@users.noreply.github.com>
2022-01-21 09:01:54 -0300

    change package hierarchy

--- setup.py
+++ setup.py
@@ -4,16 +4,15 @@ from distutils.core import setup
 
 setup(
     name='py_misc',
-    version='2.0.2',
+    version='2.1.0',
     license='MIT',
     description='Python Miscellaneous Library',
     author='anthony',
     url='https://github.com/melon-yellow/py-misc',
     packages=[
         'py_misc',
-        'py_misc/modules',
-        'py_misc/modules/call',
-        'py_misc/modules/threading'
+        'py_misc/call',
+        'py_misc/threading'
     ],
     install_requires=[
         'flask',
