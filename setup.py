f969c827330d2fb86e0752092a1811cbe8f3cd1f
rendezvous <78743473+un3481@users.noreply.github.com>
2022-12-12 11:11:06 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,17 @@ from distutils.core import setup
 
 setup(
     name='py_misc',
-    version='2.1.1',
+    version='2.2.0',
     license='MIT',
     description='Python Miscellaneous Library',
     author='anthony',
-    url='https://github.com/melon-yellow/py-misc',
+    url='https://github.com/un3481/py-misc',
     packages=[
         'py_misc',
         'py_misc/call',
         'py_misc/threading'
     ],
     install_requires=[
-        'flask',
-        'flask_httpauth',
         'schedule'
     ]
 )
